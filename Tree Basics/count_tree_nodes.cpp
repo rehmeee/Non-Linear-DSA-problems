f71a71abd2e3d1a85aa80849693719a52ec71853
rehmeee <chrehmanali5@gmail.com>
2025-07-15 11:02:38 +0500

    Count tree nodes

---
+++ Tree Basics/count_tree_nodes.cpp
@@ -0,0 +1,35 @@
+#include<iostream>
+#include<stdio.h>
+#include<string>
+#include<algorithm>
+#include<vector>
+#include<set>
+using namespace std;
+        class TreeNode{
+        public:
+        int val;
+        TreeNode *left;
+        TreeNode *right;
+        TreeNode(int val){
+        this->val = val;
+        left = nullptr;
+        right = nullptr;
+        }
+        };	
+        int countNodes(TreeNode* root){
+            if(!root) return 0;
+            return 1 + countNodes(root->left) + countNodes(root->right);
+        }
+int main(){
+        TreeNode *root = new TreeNode(1);
+        root->left = new TreeNode(2);
+        root->right = new TreeNode(3);
+        root->left->left = new TreeNode(4);
+        root->left->right = new TreeNode(5);
+        root->right->left = new TreeNode(6);
+        root->right->right = new TreeNode(7);
+        root->left->left->left = new TreeNode(8);
+        root->left->left->right = new TreeNode(9);
+        root->left->left->right->right = new TreeNode(10);	
+        cout<<countNodes(root)<<endl;
+}
